fc70cf9c352dce1a55be75b38f2c24fd5b9a243d
Akmal225d <islombahodirov424@gmail.com>
2024-12-23 17:00:21 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Contacts from "./components/Contacts/Contacts";
+import Intruduction from "./components/Introduction/Intruduction";
+import Tarifs from "./components/Tarifs/Tarifs";
+import Used from "./components/Used/Used";
+import Level from "./components/Level/Level";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Contacts />
+      <Intruduction />
+      <Tarifs />
+      <Used />
+      <Level />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Tarifs/Tarifs.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import s from './Tarifs.module.scss';
+import Data from './Data.js';
+import Card from '../Card/Card.jsx';
+
+const Tarifs = () => {
+  return (
+    <>
+      <section className={s.tarifs}>
+        <div className="container">
+          <div className={s.wrap}>
+            {Data.map((item, index) => (
+              <Card
+                key={index}
+                title={item.title}
+                text={item.text}
+                image={item.image}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Tarifs;
